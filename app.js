cba0016a88bd740c63e404a75277c9cec2119df4
Vikas Kumar <vsrathod39@gmail.com>
2022-01-20 12:09:14 +0530

    server updated

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors");
 dotenv.config({ path: "./config.env" });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(require("./router/auth"));
 
