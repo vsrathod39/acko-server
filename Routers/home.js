60d3dc958497aaf6f29d389c440c03883fe900b6
Vikas Kumar <vsrathod39@gmail.com>
2022-01-23 02:53:56 +0530

    fixing server start

--- Routers/home.js
+++ Routers/home.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-router.route("/pic").get((req, res) => {
+router.route("").get((req, res) => {
   return res.status(200).send("Welcome to home route!");
 });
 
